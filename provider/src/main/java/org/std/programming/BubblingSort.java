44f28dc8fabfca513317d906f515889101810209
DELL <DELL@mengfandong>
2019-08-21 21:05:20 +0800

    快速排序

--- provider/src/main/java/org/std/programming/BubblingSort.java
+++ provider/src/main/java/org/std/programming/BubblingSort.java
@@ -2,15 +2,44 @@ package org.std.programming;
 
 public class BubblingSort {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 9, 16, 4, 3};
+		int[] arr = {1, 12, 9, 16, 4, 3};
 //		bubbling(arr);
-		selectiveSort(arr);
+//		selectiveSort(arr);
+		
+		quickSort(arr, 1, arr.length-1);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 	}
 	
+	private static void quickSort(int[] a, int low, int height) {
+		if (low < height) {
+            int point;
+            point = partition(a, low, height);// 查找中间点
+            quickSort(a, low, point - 1);// 递归排序左边
+            quickSort(a, point + 1, height);// 递归排序右边
+        }
+	}
 	
+	private static int partition(int[] a, int low, int height) {
+        int point = a[low];// 把中间点保存在point中
+        while (low < height) {// 循环条件是只要low<height
+            // 以下两个while就是核心之处
+            while (low < height && a[height] >= point) {// low<height就不说了，本来是a[height]<poin就移动，
+                                                        // 现在改变一下
+                                                        // 当》=的时候就--；
+                height--;
+            }
+            a[low] = a[height]; // 循环结束后，交换位置，把右边小的，交换到中间点的左边
+            while (low < height && a[low] <= point) { // 相反同上， 本来是a[low]>=point
+                                                        // 改变写法 改成a<=point就跳过++；
+                low++;
+            }
+            a[height] = a[low];
+        }
+        a[low] = point;
+        return low;
+	}
 	/**
 	 *  选择排序
 	 * @param arr
